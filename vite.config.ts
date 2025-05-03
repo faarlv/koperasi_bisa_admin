62662e9974912d8b180b66681e7c5db635c101c9
faarlv <rhfa.fds@gmail.com>
2025-05-04 01:29:24 +0700

    change

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,24 @@ export default defineConfig({
   define: {
     'process.env': {}
   },
-  plugins: [react()],
+  plugins: [react({
+    jsxImportSource: 'react',
+    jsxRuntime: 'automatic'
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     }
   },
   optimizeDeps: {
-    include: ['lucide-react']
+    include: ['lucide-react','@tanstack/react-query']
   },
   build: {
     commonjsOptions: {
       include: [/lucide-react/]
+    },
+    rollupOptions: {
+      external: ['@rollup/rollup-linux-x64-gnu']
     }
   }
 });
